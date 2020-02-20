679ba4be6825da01770d14bde6711cae4fed1c4e
YutaroTakei <yutarotakei@icloud.com>
2020-02-20 22:43:08 +0900

    add new file

---
+++ sns/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_owner')
+    content = models.TextField(max_length=1000)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.content) + ' (' + str(self.owner) + ')'
+
+    class Meta:
+        ordering = ('-pub_date',)
